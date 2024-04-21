1c1cf12f7e12475ae50a7901043d48f7d95e9afd
MeoJust <meojust@gmail.com>
2024-04-21 18:50:34 +0300

    init-commit

---
+++ coffeeOneLavConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace coffeeOneLavConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+            LoyaltyCard card = new LoyaltyCard();
+
+            card.CreateClass("3388000000022315715", "coffeOneLav");
+            card.PatchClass("3388000000022315715", "coffeOneLav");
+            card.CreateObject("3388000000022315715", "coffeOneLav", "coffeOneLav01");
+            card.PatchObject("3388000000022315715", "coffeOneLav01");
+            card.CreateJWT("3388000000022315715", "coffeOneLav", "coffeOneLav01");
+        }
+    }
+}
